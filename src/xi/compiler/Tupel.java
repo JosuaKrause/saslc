0b16f9d74b36d69dbae6362293f708a041d37a1e
Joschi <josua.krause@googlemail.com>
2011-02-05 22:35:00 +0100

    added Tupels to the Grammar

---
+++ src/xi/compiler/Tupel.java
@@ -0,0 +1,39 @@
+package xi.compiler;
+
+import xi.sk.SKVisitor;
+
+public class Tupel extends Value {
+
+    public Tupel(final Expr e) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public Tupel(final Expr e, final Tupel t) {
+
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void traverse(final SKVisitor v) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
